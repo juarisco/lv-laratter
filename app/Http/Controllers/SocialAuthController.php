92c4df9684e3f513cc9b0fa625b61601496a2a56
juarisco <juarisco@outlook.com>
2019-04-01 16:03:33 -0500

    25. Login con Facebook

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -9,14 +9,34 @@ use Laravel\Socialite\Facades\Socialite;
 
 class SocialAuthController extends Controller
 {
+    /**
+     * Redirect the user to the Facebook authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function facebook()
     {
         return Socialite::driver('facebook')->redirect();
     }
 
+    /**
+     * Obtain the user information from Facebook.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function callback()
     {
         $user = Socialite::driver('facebook')->user();
+
+        $existing = User::whereHas('socialProfiles', function ($query) use ($user) {
+            $query->where('social_id', $user->id);
+        })->first();
+
+        if ($existing != null) {
+            auth()->login($existing);
+
+            return redirect('/');
+        }
 
         session()->flash('facebookUser', $user);
 
