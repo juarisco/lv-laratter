40fc05e6a914358bc78bfc1fdc29041b5821c952
juarisco <juarisco@outlook.com>
2019-04-07 20:52:35 -0500

    Emapazndo con heroku 3

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -23,8 +23,8 @@ class MessagesController extends Controller
         $message = Message::create([
             'user_id' => $user->id,
             'content' => $request->message,
-            'image' => $image->store('messages', 'public')
-            // 'image' => 'http://lorempixel.com/600/338?' . mt_rand(0, 100)
+            // 'image' => $image->store('messages', 'public')
+            'image' => 'http://lorempixel.com/600/338?' . mt_rand(0, 100)
         ]);
 
         return redirect('/messages/' . $message->id);
